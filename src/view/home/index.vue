168b51d32ba97243a632c13944345917fe1480bf
亮亮 <739547506@qq.com>
2022-01-04 22:30:39 +0800

    添加vue-router，添加默认样式库

---
+++ src/view/home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="outer_box">
+	<h1>首页</h1>
+	<button>按钮</button>
+	<input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hoem', // 首页
+  data() {
+   return {}
+ },
+  created () {},
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped>
+</style>
